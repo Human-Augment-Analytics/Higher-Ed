6c2c5e01795964c61fb0a06fc4447d54b47271ac
KaileyCozart <kaileycozart@gmail.com>
2024-05-22 13:26:55 -0700

    Recursive File Checking

--- Personal Folders/KaileyCozart/Week 2 Code Submission (05.24.24)/Automated Tag Reading/read_tags.py
+++ Personal Folders/KaileyCozart/Week 2 Code Submission (05.24.24)/Automated Tag Reading/read_tags.py
@@ -6,21 +6,31 @@ owner = 'Human-Augment-Analytics'
 repo = 'HAAG-Scripts-Repo'
 path = r'Personal Folders/KaileyCozart/Week 2 Code Submission (05.24.24)/Automated Tag Reading'
 
-url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
-url = f'https://api.github.com/repos/{owner}/{repo}/contents'
-response = requests.get(url)
-files = response.json()
+# url_specific = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+# url = f'https://api.github.com/repos/{owner}/{repo}/contents'
+# response = requests.get(url)
+# files = response.json()
 
 # Replace with your marker
 marker = '# TESTED AND DOCUMENTED'
 
-for file in files:
-    if file['type'] == 'file':
-        file_url = file['url']
-        file_response = requests.get(file_url)
-        content = base64.b64decode(file_response.json()['content']).decode('utf-8')
+def check_directory(path):
+    # url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+    url = f'https://api.github.com/repos/{owner}/{repo}/contents'
+    response = requests.get(url)
+    files = response.json()
 
-        if marker in content:
-            print(f"The file {file['path']} is tested and finalized.")
-        else:
-            print(f"The file {file['path']} is not tested and finalized.")
+    for file in files:
+        if file['type'] == 'file':
+            file_url = file['url']
+            file_response = requests.get(file_url)
+            content = base64.b64decode(file_response.json()['content']).decode('utf-8')
+
+            if marker in content:
+                print(f"The file {file['path']} is tested and finalized.")
+            else:
+                print(f"The file {file['path']} is not tested and finalized.")
+        elif file['type'] == 'dir':
+            check_directory(file['path'])
+
+check_directory(path)
