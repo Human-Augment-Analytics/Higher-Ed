5cfd46a3d9154df0dd16233ee58340686014e094
KaileyCozart <kaileycozart@gmail.com>
2024-05-22 13:21:08 -0700

    Fixed Code

--- Personal Folders/KaileyCozart/Week 2 Code Submission (05.24.24)/Automated Tag Reading/read_tags.py
+++ Personal Folders/KaileyCozart/Week 2 Code Submission (05.24.24)/Automated Tag Reading/read_tags.py
@@ -9,10 +9,9 @@ path = r'Personal Folders/KaileyCozart/Week 2 Code Submission (05.24.24)/Automat
 url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
 response = requests.get(url)
 files = response.json()
-content = base64.b64decode(response.json()['content']).decode('utf-8')
 
 # Replace with your marker
-marker = '# TESTED AND DOCUMENTED'
+marker = '# TESTED AND FINALIZED'
 
 for file in files:
     if file['type'] == 'file':
